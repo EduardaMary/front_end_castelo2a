712ac151ab53cfc9335ff5570ce9e2a227e492ee
EduardaMary <125267822+EduardaMary@users.noreply.github.com>
2023-08-14 10:38:33 -0300

    Add files via upload

---
+++ atividade_14_08/programa.js
@@ -0,0 +1,14 @@
+
+const formProdutop =
+    document.getElementById("formProduto");
+const formProdutop =
+    document.getElementById("produtosLista");
+
+    formProdutop.
+        addEventListener('submit', addProduto);
+
+const produtos = [];
+
+function addProduto(event){
+    
+}
